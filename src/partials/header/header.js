7e0f527d151fd6793bd77a73c7cae29db99c4d57
Eldar <eldar.hidirow@gmail.com>
2020-02-20 15:22:17 +0300

    исправил баг со стрелками

--- src/partials/header/header.js
+++ src/partials/header/header.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $('.header__burger').click(function () {
         $('.header__burger, .header__menu').toggleClass('active');
-        $('.arrow-down').removeClass('arrow-down');
+        $('.header .arrow-down').removeClass('arrow-down');
         $('.button--sign').css('margin-right', '15px');
     });
 })
